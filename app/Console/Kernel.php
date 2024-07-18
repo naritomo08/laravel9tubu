c91dbfa84833a98c9adfc4563e417e3597cb246e
naritomo08 <naritomo08@gmail.com>
2024-07-18 15:20:00 +0900

    upload

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('mail:send-daily-tweet-count-mail')
-            ->dailyAt('15:30');
+            ->dailyAt('7:00');
     }
 
     /**
